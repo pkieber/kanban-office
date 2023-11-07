73fb6d8d91b6b5085c9349315f6f9e9eef21e1b4
pkieber <113725311+pkieber@users.noreply.github.com>
2023-11-07 17:04:19 +0100

    update routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MainComponent } from './layout/main/main.component';
+import { SidenavSummaryComponent } from './pages/sidenav-summary/sidenav-summary.component';
 
-const routes: Routes = [];
-
+const routes: Routes = [
+  { path: '', component: MainComponent,
+    children: [
+      { path: 'sidenav-summary', component: SidenavSummaryComponent },
+    ],
+  },
+];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
